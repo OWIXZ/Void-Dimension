0c629e3ed7a9df84cd3400bc87892d3fca510e61
OWIXZ <162572367+OWIXZ@users.noreply.github.com>
2024-05-22 15:36:56 +0200

    transitions add

--- Assets/Scripts/Menu/LoadScene.cs
+++ Assets/Scripts/Menu/LoadScene.cs
@@ -1,14 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
+public class FinishPoint : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Charge la scène spécifiée
+            SceneController.instance.NextLevel();
         }
     }
 }
